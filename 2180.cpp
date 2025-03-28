404a0dfbbc941a35798a1392aaaa95f52029b3a8
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-03-28 22:33:30 +0100

    Create 2180.cpp

---
+++ 2180.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countEven(int num) {
+        int cnt = 0;
+        for (int i = 2; i <= num; i++) {
+            int sum = 0;
+            int n = i;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            if (sum % 2 == 0) cnt++;
+        }
+        return cnt;
+    }
+};
