392b568b112e0fb181604eb7067a643dfe7ca226
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-03-30 20:50:50 +0200

    Create 1528.cpp

---
+++ 1528.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string restoreString(string& s, vector<int>& indices) {
+        string str(s.length(), ' ');
+        for (int i = 0; i < s.length(); i++) str[indices[i]] = s[i];
+        return str;
+    }
+};
