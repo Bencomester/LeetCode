47abd003b41eb124ac762818c2258661dd95ebbd
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-04-03 14:16:00 +0200

    Update 2873.cpp

--- 2873.cpp
+++ 2873.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        long long val = 0;
         int len = nums.size();
-        for (int i = 0; i < len - 2; i++) {
-            for (int j = i + 1; j < len - 1; j++) {
-                int temp = nums[i] - nums[j];
-                for (int k = j + 1; k < len; k++) {
-                    val = max(val, (long long) temp * nums[k]);
-                }
-            }
+
+        vector<int> max1(len, 0);
+        int pos = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[pos] <= nums[i]) pos = i;
+            max1[i] = pos;
+        }
+
+        vector<int> max2(len, 0);
+        pos = len - 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if (nums[pos] <= nums[i]) pos = i;
+            max2[i] = pos;
+        }
+
+        long long val = 0;
+        for (int i = 1; i < len - 1; i++) {
+                val = max(val, (long long) (nums[max1[i - 1]] - nums[i]) * nums[max2[i + 1]]);
         }
         return val;
     }
