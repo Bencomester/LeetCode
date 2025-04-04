502809f38eadd1abbf7b5edd6191c9eda7af600c
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-04-04 20:42:02 +0200

    Create 0865.cpp

---
+++ 0865.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int l = findDepth(root->left);
+        int r = findDepth(root->right);
+        if (l == r) return root;
+        if (l > r) return subtreeWithAllDeepest(root->left);
+        return subtreeWithAllDeepest(root->right);
+    }
+
+    int findDepth(TreeNode* root, int depth = 0) {
+        if (!root) return depth;
+        return max(findDepth(root->left, depth + 1), findDepth(root->right, depth + 1));
+    }
+};
