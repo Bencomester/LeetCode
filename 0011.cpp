97b8111023c01b2c27f8e23ace44301f99e396b6
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-04-05 08:57:36 +0200

    Update 0011.cpp

--- 0011.cpp
+++ 0011.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int len = height.size();
         int water = 0;
-        vector<int> maxs(len, height[len - 1]);
-        for (int i = len - 2; i >= 0; i--) maxs[i] = max(maxs[i + 1], height[i]);
-        for (int i = 0; i < len - 1; i++) {
-            int w = len - i - 1;
-            if (maxs[i] * w < water) break;
-            if (height[i] * w < water) continue;
-            for (int j = i + 1; j < len; j++) {
-                water = max(water, min(height[i], height[j]) * (j - i));
-            }
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            water = max(water, (right - left) * min(height[right], height[left]));
+            if (height[left] > height[right]) right--;
+            else left++;
         }
         return water;
     }
