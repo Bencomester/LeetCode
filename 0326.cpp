322ff5b3b53667cc1c44a4539f5dd06ce9beb0ea
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-08-13 09:16:10 +0200

    Create 0326.cpp

---
+++ 0326.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while (n > 1) {
+            if (n % 3) return false;
+            else n /= 3;
+        }
+        return n == 1;
+    }
+};
